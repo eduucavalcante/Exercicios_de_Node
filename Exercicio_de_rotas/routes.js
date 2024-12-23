7a4f0e5f43b521234e1da287126b4d9c6ba98345
Eduardo Cavalcante <eduardo.cavalcante.contact@gmail.com>
2024-12-23 15:28:40 -0300

    Primeira commit de exercício

---
+++ Exercicio_de_rotas/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	res.status(200).send('<h1>Exercicio de rotas com Node.js e Express.js</h1><a href="http://localhost:8081/sobre" target="_self">Página Sobre</a>');
+});
+
+router.get('/sobre', (req, res) => {
+	res.status(200).send('Este repositorio tem a finalidade de praticar durante os estudos de Node.js');
+});
+
+module.exports = router;
